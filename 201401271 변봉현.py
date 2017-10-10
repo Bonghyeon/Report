fc79f8503e3c176f084106f1c64f76cc20ca50f3
Bonghyeon <31956129+Bonghyeon@users.noreply.github.com>
2017-10-10 16:18:10 +0900

    과제

---
+++ 201401271 변봉현.py
@@ -0,0 +1,23 @@
+import turtle as t
+t.shape("turtle")
+t.speed(0)
+t.bgcolor("black")
+t.color("green")
+for x in range (150):
+    t.forward(x)
+    t.left(90)
+    t.forward(x)
+    t.left(90)
+    t.forward(x)
+    t.right(90)
+    t.forward(x)
+    t.left(90)
+    t.forward(x)
+    t.left(90)
+    t.forward(x)
+    t.right(90)
+    t.forward(x)
+    t.left(90)
+    t.forward(x+x)
+
+    
